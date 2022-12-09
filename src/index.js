7560eaf4ff45af7abeb7fc7e39d8ff526f8bf5f8
PotapenkoVadim <potapenko.v00@mail.ru>
2022-12-09 12:17:00 +0300

    feat: implement mobile menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './index.html';
 import './styles/main.scss';
+
+const ACTIVE_MOBILE_MENU_CLASSNAME = 'header__navigation_open';
+const mobileMenu = document.getElementById('mobileMenu');
+const mobileCloseIcon = document.getElementById('mobileCloseIcon');
+const mobileNavigation = document.getElementById('mobileNavigation');
+
+mobileMenu.addEventListener('click', () => {
+  mobileNavigation.classList.toggle(ACTIVE_MOBILE_MENU_CLASSNAME);
+});
+
+mobileCloseIcon.addEventListener('click', () => {
+  mobileNavigation.classList.remove(ACTIVE_MOBILE_MENU_CLASSNAME);
+});
